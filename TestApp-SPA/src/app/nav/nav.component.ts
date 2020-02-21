ee3f8a6cfbd9880ad50283a002d1e831bca658ba
martinloord <43519485+martinloord@users.noreply.github.com>
2020-02-21 09:46:17 +0100

    end of section 7

--- TestApp-SPA/src/app/nav/nav.component.ts
+++ TestApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor( public authService: AuthService, private alertify: AlertifyService) { }
+  constructor( public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,7 +23,10 @@ export class NavComponent implements OnInit {
         this.alertify.success('Logged in successfully');
       }, error => {
         this.alertify.error(error);
-      });
+      }, () => {
+        this.router.navigate(['/member']);
+      }
+      ); // efter att man har gjort routes.ts och länkat i html, lägg till Router i constructorn och den länka i koden här ovan
     }
 
     loggedIn() {
@@ -32,5 +36,6 @@ export class NavComponent implements OnInit {
     loggOut() {
       localStorage.removeItem('token');
       this.alertify.message('Logged out');
+      this.router.navigate(['/home']);
     }
 }
